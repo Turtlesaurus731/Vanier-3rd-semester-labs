c945919940b71d91e14e9d67ebf873ac81249c5e
Patrick Tran <patrick.huy.tran@gmail.com>
2026-09-02 20:18:19 -0400

    Configured spacing and added buttons

--- Lab03/src/main/java/com/VanierPatrick/lab03/App.java
+++ Lab03/src/main/java/com/VanierPatrick/lab03/App.java
@@ -1,7 +1,9 @@
 package com.VanierPatrick.lab03;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
@@ -18,20 +20,25 @@ public class App extends Application {
     @Override
     public void start(Stage stage) {
         BorderPane root = new BorderPane();
-        GridPane gridPane = new GridPane(2, 7);
+        GridPane gridPane = new GridPane();
         root.setCenter(gridPane);
         gridPane.setHgap(10);
         gridPane.setVgap(10);
+        gridPane.setPadding(new Insets(20));
         
         Label firstName = new Label("FirstName");
         Label LastName = new Label("LastName");
         Label email = new Label("Email");
         Label password = new Label("Password");
         
-        TextField firstNameField = new TextField("");
-        TextField lastNameField = new TextField("");
-        TextField emailField = new TextField("");
-        TextField passwordField = new TextField("");
+        TextField firstNameField = new TextField();
+        TextField lastNameField = new TextField();
+        TextField emailField = new TextField();
+        TextField passwordField = new TextField();
+        
+        Button registerBtn = new Button("Register");
+        Button clearBtn = new Button("Clear");
+        Label statusLabel = new Label(""); 
         
         gridPane.add(firstName, 0, 0);
         gridPane.add(firstNameField, 1, 0);
@@ -42,8 +49,12 @@ public class App extends Application {
         gridPane.add(password, 0, 3);
         gridPane.add(passwordField, 1, 3);
         
+        gridPane.add(registerBtn, 0, 4);
+        gridPane.add(clearBtn, 1, 4);
+        gridPane.add(statusLabel, 0, 5, 2, 1); 
         
-        Scene scene = new Scene(root, 600, 300);
+        
+        Scene scene = new Scene(root, 400, 300);
         stage.setScene(scene);
         stage.show();
     }
