e0cb3c7967bf9162cb829678f323ae9698491ee4
Patrick Tran <patrick.huy.tran@gmail.com>
2026-09-14 18:59:20 -0400

    Updated InputValidator to handle more cases
    Input validator will now return false when given a negative data, when days are equal to 0 and when given an empty field

--- Lab04/src/main/java/com/mycompany/lab04/InputValidatior.java
+++ Lab04/src/main/java/com/mycompany/lab04/InputValidatior.java
@@ -16,14 +16,28 @@ public class InputValidatior {
     public static boolean validate(Map<String, TextField> userInformation) {
         for (String fieldName: userInformation.keySet()) {
             TextField field = userInformation.get(fieldName);
-            String infoToValidate = field.getText();
-            infoToValidate.trim();
+            String infoToValidate = field.getText().trim();
+            
+            if (infoToValidate.isEmpty()) {
+                System.out.println(fieldName + "is empty");
+                return false;
+            }
             
             try {
                 if (fieldName.equalsIgnoreCase("Days")) {
-                    Integer.valueOf(infoToValidate);
+                    int days = Integer.valueOf(infoToValidate);
+                    
+                    if (days <= 0) {
+                        System.out.println(fieldName + "is not greater than 0");
+                        return false;
+                    }
                 } else {
-                    Double.valueOf(infoToValidate);
+                    double amount = Double.valueOf(infoToValidate);
+                    
+                    if (amount < 0) {
+                        System.out.println(fieldName + " can't be negative");
+                        return false;
+                    }
                 }
             } catch (NumberFormatException e) {
                 System.out.println(fieldName + " is invalid");
