81c3c1bc1b9d63af44724168fec3c5f5cea3519b
Patrick Tran <patrick.huy.tran@gmail.com>
2026-09-14 08:57:17 -0400

    Initialize scene and added GridPane
     Initial scene with BorderPane and empty GridPane

--- Lab04/src/main/java/com/mycompany/lab04/App.java
+++ Lab04/src/main/java/com/mycompany/lab04/App.java
@@ -1,8 +1,11 @@
 package com.mycompany.lab04;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -14,11 +17,14 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-        var javaVersion = SystemInfo.javaVersion();
-        var javafxVersion = SystemInfo.javafxVersion();
-
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
+        BorderPane root = new BorderPane();
+        GridPane gridPane = new GridPane();
+        root.setCenter(gridPane);
+        gridPane.setHgap(10);
+        gridPane.setVgap(10);
+        gridPane.setPadding(new Insets(20));
+        
+        Scene scene = new Scene(root, 400, 300);
         stage.setScene(scene);
         stage.show();
     }
