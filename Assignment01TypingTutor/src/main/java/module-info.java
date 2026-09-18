7e0f805f5c25d21f724d078a1c898f881a6bd3ba
Patrick Tran <patrick.huy.tran@gmail.com>
2026-09-18 18:25:38 -0400

    Initial commit

---
+++ Assignment01TypingTutor/src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module vanierpatrick.assignment01typingtutor {
+    requires javafx.controls;
+    exports vanierpatrick.assignment01typingtutor;
+}
---
+++ Assignment01TypingTutor/src/main/java/vanierpatrick/assignment01typingtutor/SystemInfo.java
@@ -0,0 +1,13 @@
+package vanierpatrick.assignment01typingtutor;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
