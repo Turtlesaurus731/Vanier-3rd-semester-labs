ef1bf377bb27564ff8c16b008edf3db6b509a9ac
Patrick Tran <patrick.huy.tran@gmail.com>
2026-09-19 22:42:30 -0400

    Create KeyboardKeys.java

---
+++ Assignment01TypingTutor/src/main/java/vanierpatrick/assignment01typingtutor/KeyboardKeys.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package vanierpatrick.assignment01typingtutor;
+
+import javafx.scene.shape.QuadCurve;
+
+/**
+ *
+ * @author patri
+ */
+public enum KeyboardKeys {
+    Q, W, E, R, T, Y, U, I, O, P,
+    A, S, D, F, G, H, J, K, L,
+    SHIFT, Z, X, C, V, B, N, M,
+    SPACE
+}
